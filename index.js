9e14d51551ed3d6572fe364a7c0776af41d59810
aliciasaci <lyciiasci@gmail.com>
2022-11-23 00:01:35 +0100

    Ajout d'un linter JS et de prettier pour formater le code
     Ajout d'un linter JS 'eliner' dans un GIT Hook pour détécter les potentils erreurs et prettier pour formater le code #9

---
+++ index.js
@@ -0,0 +1,14 @@
+const y =0;
+
+const x =3;
+// const string = x * x;
+// const cubed = x * x * x;
+
+let resultat = x + y;
+
+if (resultat) {
+  console.log(resultat);
+} else {
+  console.log("nothing");
+}
+alert("foo");
